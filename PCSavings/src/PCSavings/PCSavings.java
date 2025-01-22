7d2a734e55165a67df902b2843892b0378fbebeb
SWells-77 <sp.sweetapple@gmail.com>
2025-01-22 17:18:00 -0500

    Here's Lab 1

---
+++ PCSavings/src/PCSavings/PCSavings.java
@@ -0,0 +1,11 @@
+
+
+public static void main(String[] args) {
+    int Price = 2275;
+    int Savings = 50;
+    double Weeks = 45.5;
+
+    System.out.println("The computer you want costs:" + Price);
+    System.out.println("You are able to save:" + Savings);
+    System.out.println("This is how many weeks it will take to save for your computer:" + Weeks);}
+
